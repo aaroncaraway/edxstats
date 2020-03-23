066e94821dbd281b29315303ea24336f0c906fe4
aaroncaraway <aaroncaraway42@gmail.com>
2020-03-23 10:06:56 -0700

    Adding resource

--- EDXStats_ch4_notes.R
+++ EDXStats_ch4_notes.R
@@ -10,5 +10,7 @@ top <- exp(-10.6513 + 0.0055*n)
 bottom <- 1 + exp(-10.6513 + 0.0055*n)
 top/bottom
 
+# WOBSITES
+# https://www.mathsisfun.com/numbers/e-eulers-number.html
 
 # We know that logit(.5) =  𝛽0+𝛽1 *Balance. Thus, Balance = (logit(.5) -  𝛽0 )/ 𝛽1  = (log(.5/(1-.5)) + 10.6513)/.0055 = 1936.6
