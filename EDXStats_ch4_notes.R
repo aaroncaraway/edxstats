9d22ebc8adfd92eea845acd37a31429035320014
aaroncaraway <aaroncaraway42@gmail.com>
2020-03-23 09:40:46 -0700

    Ch4 notes

---
+++ EDXStats_ch4_notes.R
@@ -0,0 +1,14 @@
+n <- 1940 
+B0 <- -10.6513 # Coefficient intercept 
+B1 <- 0.0055 # Other intercept 
+top <- exp(B0 + B1 * n)
+bottom <- 1 + exp(B0 + B1 * n)
+top/bottom
+
+n <- 1940
+top <- exp(-10.6513 + 0.0055*n)
+bottom <- 1 + exp(-10.6513 + 0.0055*n)
+top/bottom
+
+
+# We know that logit(.5) =  𝛽0+𝛽1 *Balance. Thus, Balance = (logit(.5) -  𝛽0 )/ 𝛽1  = (log(.5/(1-.5)) + 10.6513)/.0055 = 1936.6
