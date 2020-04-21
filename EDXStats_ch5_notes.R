ef1b737d6fe28b37b9ccb2a8066f8027fc6f44c8
aaroncaraway <aaroncaraway42@gmail.com>
2020-04-21 13:02:00 -0700

    Adding lab 5.3

--- EDXStats_ch5_notes.R
+++ EDXStats_ch5_notes.R
@@ -48,20 +48,21 @@ lines(degree, cv.error10, type="b",col="red")
 #################################
 
 
-alpha=function(x,y){
+alpha.fn=function(x,y){
   vx=var(x)
   vy=var(y)
   cxy=cov(x,y)
   (vy-cxy)/(vx+vy-2*cxy)
 }
 
+alpha.fn(Portfolio,1:100)
+set.seed(1)
+alpha.fn (Portfolio,sample(1:100,100,replace=TRUE))
 
-
-
-
-
-
-
+# 1000 bootstraps
+boot.out=boot(Portfolio, alpha.fn,R=1000)
+boot.out
+plot(boot.out)
 
 
 ## QUIZ QUESTIONS
@@ -71,7 +72,30 @@ glm.fit=glm(y~X1+X2, data=Xy)
 
 summary(glm.fit)
 
-
-
-
 matplot(Xy,type="l")
+
+### QUESTION THREE
+
+alpha=function(x,y){
+  vx=var(x)
+  vy=var(y)
+  cxy=cov(x,y)
+  (vy-cxy)/(vx+vy-2*cxy)
+}
+# alpha(Portfolio$X,Portfolio$Y)
+alpha(Xy$X1,Xy$X2)
+
+
+alpha.fn(Xy,1:100)
+set.seed(1)
+alpha.fn(Xy,sample(1:100,100,replace=TRUE))
+
+# 1000 bootstraps
+boot.out=boot(Xy, alpha.fn,R=1000)
+boot.out
+plot(boot.out)
+summary(boot)
+
+new.rows = c(101:200, 401:500, 101:200, 901:1000, 301:400, 1:100, 1:100, 801:900, 201:300, 701:800)
+
+new.Xy = Xy[new.rows, ]
